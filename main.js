77380f2bc59c5a7555b5566e51845829eb9367dd
Preston Allred <43273024+pred12@users.noreply.github.com>
2020-04-18 20:50:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-/*
-!!!
-READ THE STUFF BELOW
-
-Don't hit command/ctrl A click and drag/highlight all of it or else Github will put a lot of extra stuff
-If you want more more/less colors or more/less time between colors please email me! (123097@usd230.org) and put the subject-
-as "Background-Color:Edit" thanks
-or just change the color and time your self!
-to change the color  ["Color1", "color2"]; use normal colors that are well knowed or look up "css colors"
-!!!
-*/
-
-//start copying the stuff below (click and drag/highlight)
-
 const color = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige", "Bisque", "Black", "BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","Navy","OldLace","Olive","OliveDrab","Orange","PeachPuff","Peru", "Pink","Plum","PowderBlue","Purple","RebeccaPurple","Sienna","Silver","SkyBlue","SlateBlue", "SlateGray","SlateGrey","Snow"];
 const time = 2000 //in milliseconds
 const randomColor = true; //if you want the colors to go in order or be random true for random false for not random
@@ -30,13 +16,7 @@ async function change(){
       
   } else{
     document.querySelector("body").style.backgroundColor = color[i];
-    //this is if 'i' is greater than the length of color
     i++;
   } 
 }
-//Starts the color changing
-
-//stop copying stuff
-
-
 //By Preston Allred
